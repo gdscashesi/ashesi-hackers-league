e452f30237815e4f89682bf5199672b0f77163d5
Faddal Ibrahim <faddalibrahim@gmail.com>
2022-07-26 21:47:48 +0000

    fixed conflicts

--- src/pages/errors/error_boundary/ErrorBoundary.js
+++ src/pages/errors/error_boundary/ErrorBoundary.js
@@ -1,45 +1,44 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 //components
-import ErrorBoundarySvg from './ErrorBoundarySvg';
+import ErrorBoundarySvg from "./ErrorBoundarySvg";
 
 //styles
-import errorBoundaryStyles from "./error-boundary.module.scss"
+import errorBoundaryStyles from "./error-boundary.module.scss";
 
-export class ErrorBoundary extends Component {  
-    constructor(props) {  
-      super(props);  
-      this.state = { error: null, errorInfo: null };  
-    }  
-      
-    componentDidCatch(error, errorInfo) {  
-      this.setState({  
-        error: error,  
-        errorInfo: errorInfo  
-      })  
-      console.log(errorInfo)
-    }  
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { error: null, errorInfo: null };
+  }
 
-    render() {  
-      if (this.state.errorInfo) {  
-        return (  
-          <div className={errorBoundaryStyles.errorMountWrapper}>
+  componentDidCatch(error, errorInfo) {
+    this.setState({
+      error: error,
+      errorInfo: errorInfo,
+    });
+    console.log(errorInfo);
+  }
+
+  render() {
+    if (this.state.errorInfo) {
+      return (
+        <div className={errorBoundaryStyles.errorMountWrapper}>
           <div className={errorBoundaryStyles.header}>
             <h2>Ashesi Hackers League</h2>
           </div>
-    
+
           <div className={errorBoundaryStyles.errorMount}>
             <div className={errorBoundaryStyles.errorMountSvg}>
-             <ErrorBoundarySvg />
+              <ErrorBoundarySvg />
             </div>
-          <strong>
-            Oops! Something went wrong, we are fixing it
-          </strong>
+            <strong>Oops! Something went wrong, we are fixing it</strong>
           </div>
         </div>
-        );  
-      }  
-      return this.props.children;  
-    }    
-  }  
+      );
+    }
+    return this.props.children;
+  }
+}
 
+export default ErrorBoundary;
