ed4f85d812ca23e61a6f0bc55aa047a412658c56
Simon-157 <boatengsimonjnr157@gmail.com>
2022-07-27 23:56:55 +0000

    few changes

--- src/pages/admin/protected_routes/AdminProtectedRoute.js
+++ src/pages/admin/protected_routes/AdminProtectedRoute.js
@@ -3,6 +3,7 @@ import { Navigate, Outlet } from "react-router-dom";
 
 export const AdminProtectedRoute = () => {
 
+    //getting admin from state management, but null for now
     const admin = null;
 
     return <>
