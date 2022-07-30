45a12a40c2343fb1d8c0aa0be844a7c8e0ae25b4
Simon-157 <boatengsimonjnr157@gmail.com>
2022-07-30 20:38:50 +0000

    changed function name

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,24 +1,24 @@
 export function sortTable(table, key) {}
 
-export function getOrdinary(number){
+export function getOrdinal(number){
     //spliting number in terms of ones and tens columns
     let ones = number % 10;
     let tens = number % 100;
 
     if (ones === 1 && tens !== 11){
-        return number + "st"
+    return number + "st"
     }
 
     else if (ones === 2 && tens !==12){
-        return number + "nd"
+    return number + "nd"
     }
 
     else if (ones === 3 && tens !== 13){
-        return number + "rd"
+    return number + "rd"
     }
 
     else{
-        return number + "th"
+    return number + "th"
     }
 
 
