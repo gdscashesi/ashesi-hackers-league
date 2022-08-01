fad221272f61614f18aa82c830d4d23cc513ca24
Oheneba Dade <ohenebadade@gmail.com>
2022-08-01 22:02:46 +0000

    throw exceptions when key not found

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,11 +1,24 @@
 export function sortTable(table, key) {
-	if (key === "name") {
-		table.sort();
+	const containsKey = (object) => key in object;
+
+	if (!table.some(containsKey)) {
+		throw new Error("Table does not contain key: " + key);
+	} else if (key === "name") {
+		table.sort((a, b) => {
+			const current = a.name.toLowerCase(),
+				next = b.name.toLowerCase();
+
+			if (current < next) return -1;
+			else if (current > next) return 1;
+			else return 0;
+		});
 	}
 	table.sort((a, b) => {
 		return b[key] - a[key];
 	});
+	console.log(table);
 }
+
 export function getOrdinal(number) {
 	//spliting number in terms of ones and tens columns
 	let ones = number % 10;
