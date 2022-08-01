c665a632c105f572dd15ecef617191e65f8b88b3
Oheneba Dade <ohenebadade@gmail.com>
2022-08-01 17:59:42 +0000

    Implement sort algorithm

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,15 +1,18 @@
-export function sortTable(table, key) {}
+export function sortTable(table, key) {
+	if (key === "name") {
+		table.sort();
+	}
+	table.sort((a, b) => {
+		return b[key] - a[key];
+	});
+}
+export function getOrdinal(number) {
+	//spliting number in terms of ones and tens columns
+	let ones = number % 10;
+	let tens = number % 100;
 
-export function getOrdinal(number){
-    //spliting number in terms of ones and tens columns
-    let ones = number % 10;
-    let tens = number % 100;
-
-    if (ones === 1 && tens !== 11) return number + "st"
-    else if (ones === 2 && tens !==12) return number + "nd"
-    else if (ones === 3 && tens !== 13) return number + "rd"
-    else return number + "th"
-    
-
-
+	if (ones === 1 && tens !== 11) return number + "st";
+	else if (ones === 2 && tens !== 12) return number + "nd";
+	else if (ones === 3 && tens !== 13) return number + "rd";
+	else return number + "th";
 }
