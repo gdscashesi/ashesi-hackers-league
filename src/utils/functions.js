010618a48600aa8dad655a3d24bfb3835d8db549
Faddal Ibrahim <faddalibrahim@gmail.com>
2022-08-02 00:10:30 +0000

    add doc for functions.js

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,30 +1,44 @@
+/**
+ * sort an array of objects by a specified key in descending order
+ *
+ *  @param { Array <Objects> } table - The array of objects to be sorted
+ *  @param { string } key - The key to sort according to
+ *
+ *  @return Array <Objects>
+ */
 export function sortTable(table, key) {
-	//check for the presence of the key in the first object of the table array
-	if (!(key in table[0]))
-		throw new Error("Table does not contain key: " + key);
-	if (key === "name") {
-		table.sort((a, b) => {
-			const current = a.name.toLowerCase(),
-				next = b.name.toLowerCase();
+  if (!(key in table[0])) throw new Error("Table does not contain key: " + key);
 
-			if (current < next) return -1;
-			else if (current > next) return 1;
-			else return 0;
-		});
-	}
-	table.sort((a, b) => {
-		return b[key] - a[key];
-	});
-	console.log(table);
+  if (key === "name") {
+    table.sort((a, b) => {
+      const current = a.name.toLowerCase();
+      const next = b.name.toLowerCase();
+
+      if (current < next) return -1;
+      else if (current > next) return 1;
+      else return 0;
+    });
+  }
+
+  table.sort((a, b) => b[key] - a[key]);
+
+  return table;
 }
 
-export function getOrdinal(number) {
-	//spliting number in terms of ones and tens columns
-	let ones = number % 10;
-	let tens = number % 100;
+/**
+ * generates the ordinal of a number
+ *
+ *  @param {number} number - the number whose ordinal is to be generate
+ *
+ *  @return string
+ */
 
-	if (ones === 1 && tens !== 11) return number + "st";
-	else if (ones === 2 && tens !== 12) return number + "nd";
-	else if (ones === 3 && tens !== 13) return number + "rd";
-	else return number + "th";
+export function getOrdinal(number) {
+  let ones = number % 10;
+  let tens = number % 100;
+
+  if (ones === 1 && tens !== 11) return number + "st";
+  else if (ones === 2 && tens !== 12) return number + "nd";
+  else if (ones === 3 && tens !== 13) return number + "rd";
+  else return number + "th";
 }
