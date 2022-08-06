5a9fb1c096f311ff090eade799a2f47ec8154d64
Oheneba Dade <65047259+Oheneba-Dade@users.noreply.github.com>
2022-08-06 13:38:58 +0000

    create toggle text area functionality (#36)
    * create toggle text area functionality

---
+++ src/components/mini_side_nav/MiniSideNav.js
@@ -0,0 +1,47 @@
+//libraries
+import React, { useState } from "react";
+
+//components
+import Button from "components/button/Button";
+
+const MiniSideNav = () => {
+	const [current, setCurrent] = useState("algorithms");
+
+	const handleClick = (e) => {
+		setCurrent(e.target.innerText);
+	};
+
+	const AlgorithmsTextArea = () => {
+		return <textarea name="algorithms" placeholder="algo"></textarea>;
+	};
+	const ScriptingTextArea = () => {
+		return <textarea name="scripting" placeholder="scripting"></textarea>;
+	};
+	const SqlTextArea = () => {
+		return <textarea name="sql" placeholder="sql"></textarea>;
+	};
+
+	const questionTypes = ["algorithms", "scripting", "sql"];
+	const questionTypeComponents = {
+		algorithms: <AlgorithmsTextArea />,
+		scripting: <ScriptingTextArea />,
+		sql: <SqlTextArea />,
+	};
+
+	return (
+		<div>
+			{questionTypes.map((category) => {
+				return (
+					<Button
+						key={category}
+						text={category}
+						onClick={handleClick}
+					></Button>
+				);
+			})}
+			{questionTypeComponents[current]}
+		</div>
+	);
+};
+
+export default MiniSideNav;
--- src/pages/sandbox/Sandbox.js
+++ src/pages/sandbox/Sandbox.js
@@ -1,13 +1,13 @@
-import Simon from "./simon/Simon";
+import Oheneba from "./oheneba/Oheneba";
 
 const Sandbox = () => {
-  return (
-    <div>
-      Import the component with your respective name here for temporary testing
-      purposes. Go to <strong>/sandbox</strong> to view it
-      <Simon />
-    </div>
-  );
+	return (
+		<div>
+			Import the component with your respective name here for temporary
+			testing purposes. Go to <strong>/sandbox</strong> to view it
+			<Oheneba />
+		</div>
+	);
 };
 
 export default Sandbox;
