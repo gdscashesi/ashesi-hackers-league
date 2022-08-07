0eee1b20ce5e474ca08e381794187072742a6ec5
Simon-157 <boatengsimonjnr157@gmail.com>
2022-08-07 22:26:52 +0000

    added admin-nav

---
+++ src/pages/admin/admin_top_nav/AdminTopNav.js
@@ -0,0 +1,70 @@
+//libraries
+
+//styles
+import TopNavStyles from './admin-top-nav.module.scss'
+
+//Components
+import Button from 'components/button/Button'
+import { InsightsIcon, PublishedIcon, ReportsIcon, } from 'components/icons/Icons'
+
+//utils
+import { 
+    HOME,
+    ADMIN,
+    INSIGHTS,
+    PUBLISHED,
+    REPORTS,
+    NEW
+} from "utils/constants"
+
+
+const AdminTopNav = () => {
+  return (
+    <nav className={TopNavStyles.wrapper}>
+        <span className={TopNavStyles.newBtn}>
+            <Button 
+                text = "New"
+                to = {ADMIN + HOME + NEW}
+            />
+        </span>
+        <button className={TopNavStyles.toggleBtn}>
+            {/* <ToggleIcon /> */}
+        </button>
+        <div className={TopNavStyles.navItems}>
+            <ul>
+                <li>
+                    <Button 
+                        icon = {<InsightsIcon />}
+                        text="insights" 
+                        to={ADMIN + HOME + INSIGHTS}
+                    />
+                </li>
+                <li>
+                    <Button 
+                        icon = {<ReportsIcon />}
+                        text="reports" 
+                        to={ADMIN + HOME + REPORTS}
+                    />
+                </li>
+                <li>
+                    <Button 
+                        icon = {<PublishedIcon />}
+                        text="published" 
+                        to={ADMIN + HOME + PUBLISHED}
+                    />               
+                </li>
+                <li>
+                    <Button 
+                        icon = {<PublishedIcon />}
+                        text="extra" 
+                        to={ADMIN + HOME + PUBLISHED}
+                    />
+                    
+                </li>
+            </ul>
+        </div>
+    </nav>
+  )
+}
+
+export default AdminTopNav
