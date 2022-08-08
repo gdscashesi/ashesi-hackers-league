fa863a1db39b2b4d4ed099165c0a0ef19088be1d
Simon-157 <boatengsimonjnr157@gmail.com>
2022-08-08 02:30:51 +0000

    mapped buttons from an array

--- src/pages/admin/admin_top_nav/AdminTopNav.js
+++ src/pages/admin/admin_top_nav/AdminTopNav.js
@@ -5,55 +5,23 @@ import TopNavStyles from './admin-top-nav.module.scss'
 
 //Components
 import Button from 'components/button/Button'
-import { InsightsIcon, NewIcon, PublishedIcon, ReportsIcon, ToggleIcon } from 'components/icons/Icons'
-
-//utils
-import { 
-    HOME,
-    ADMIN,
-    INSIGHTS,
-    PUBLISHED,
-    REPORTS,
-    NEW,
-    EXTRA
-} from "utils/constants"
+import { NavigationButtons } from "./NavigationButtons"
 
 
 const AdminTopNav = () => {
     
   return (
     <nav className={TopNavStyles.wrapper}>
-        <span className={TopNavStyles.newBtn}>
-            <Button 
-                icon = {<NewIcon />}
-                text = {NEW}
-                to = {ADMIN + HOME + NEW}
-            />
-        </span>
-        <button className={TopNavStyles.toggleBtn} >
-            <ToggleIcon />
-        </button>
         <div className={TopNavStyles.navItems }>
-            <Button 
-                icon = {<InsightsIcon />}
-                text={INSIGHTS} 
-                to={ADMIN + HOME + INSIGHTS}
-            />
-            <Button 
-                icon = {<ReportsIcon />}
-                text={REPORTS}
-                to={ADMIN + HOME + REPORTS}
-            />
-            <Button 
-                icon = {<PublishedIcon />}
-                text={PUBLISHED}
-                to={ADMIN + HOME + PUBLISHED}
-            />               
-            <Button 
-                icon = {<PublishedIcon />}
-                text={EXTRA} 
-                to={ADMIN + HOME + EXTRA}
-            />
+           {
+            NavigationButtons?.map((button) =>{
+                return(
+                 <Button key={button.text} 
+                    icon={button.icon} text={button.text} to={button.to}
+                 />
+                )}
+             )
+           }
         </div>
     </nav>
   )
