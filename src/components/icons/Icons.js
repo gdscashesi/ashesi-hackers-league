c49944fc4427c02b306d44d91443012dab35b2b4
Simon-157 <boatengsimonjnr157@gmail.com>
2022-08-08 00:13:55 +0000

    changed insight icon and export format of icons

--- src/components/icons/Icons.js
+++ src/components/icons/Icons.js
@@ -1,23 +1,21 @@
-const InsightsIcon = () => {
+export const InsightsIcon = () => {
   return (
-    <svg
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M15 8H19V20H5V10H9V4H15V8ZM13 6H11V18H13V6ZM15 10V18H17V10H15ZM9 12V18H7V12H9Z"
-        fill="currentColor"
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path 
+        d="M15 7C15 6.44772 15.4477 6 16 6C16.5523 6 17 6.44772 17 7V17C17 17.5523 16.5523 18 16 18C15.4477 
+        18 15 17.5523 15 17V7Z" fill="currentColor" /><path d="M7 15C7 14.4477 7.44772 14 8 14C8.55228 14 9 
+        14.4477 9 15V17C9 17.5523 8.55228 18 8 18C7.44772 18 7 17.5523 7 17V15Z" fill="currentColor" 
+      />
+      <path 
+        d="M12 10C11.4477 10 11 10.4477 11 11V17C11 17.5523 11.4477 18 12 18C12.5523 18 13 17.5523 13 17V11C13
+        10.4477 12.5523 10 12 10Z" 
+        fill="currentColor" 
       />
     </svg>
   );
 };
 
-const ReportsIcon = () => {
+export const ReportsIcon = () => {
   return (
     <svg
       width="24"
@@ -39,7 +37,7 @@ const ReportsIcon = () => {
   );
 };
 
-const PublishedIcon = () => {
+export const PublishedIcon = () => {
   return (
     <svg
       width="24"
@@ -64,7 +62,7 @@ const PublishedIcon = () => {
   );
 };
 
-const NewIcon = () => {
+export const NewIcon = () => {
   return(
     <svg 
       width="24" 
@@ -92,7 +90,7 @@ const NewIcon = () => {
   )
 }
 
-const ToggleIcon = () => {
+export const ToggleIcon = () => {
   return(
     <svg 
       xmlns="http://www.w3.org/2000/svg" 
@@ -112,4 +110,4 @@ const ToggleIcon = () => {
   )
 }
 
-export { PublishedIcon, ReportsIcon, InsightsIcon, NewIcon, ToggleIcon };
+
