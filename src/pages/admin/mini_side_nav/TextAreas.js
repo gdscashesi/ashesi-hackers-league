c706df0e50147ef52df5d174fd1db3d8b9b4ba37
Oheneba Dade <ohenebadade@gmail.com>
2022-08-13 00:24:25 +0000

    implement retrieval of questions from localstorage (#43)

--- src/pages/admin/mini_side_nav/TextAreas.js
+++ src/pages/admin/mini_side_nav/TextAreas.js
@@ -1,21 +1,30 @@
-export const AlgorithmsTextArea = ({ onChange }) => {
-  return (
-    <textarea
-      name="algorithms"
-      placeholder="algorithms"
-      onChange={onChange}
-    ></textarea>
-  );
+export const AlgorithmsTextArea = ({ onChange, value }) => {
+	return (
+		<textarea
+			name="algorithms"
+			placeholder="algorithms"
+			onChange={onChange}
+			value={value}
+		></textarea>
+	);
 };
-export const ScriptingTextArea = ({ onChange }) => {
-  return (
-    <textarea
-      name="scripting"
-      placeholder="scripting"
-      onChange={onChange}
-    ></textarea>
-  );
+export const ScriptingTextArea = ({ onChange, value }) => {
+	return (
+		<textarea
+			name="scripting"
+			placeholder="scripting"
+			onChange={onChange}
+			value={value}
+		></textarea>
+	);
 };
-export const SqlTextArea = ({ onChange }) => {
-  return <textarea name="sql" placeholder="sql" onChange={onChange}></textarea>;
+export const SqlTextArea = ({ onChange, value }) => {
+	return (
+		<textarea
+			name="sql"
+			placeholder="sql"
+			onChange={onChange}
+			value={value}
+		></textarea>
+	);
 };
