455b49f00d79e10c268f9a405ddd274b0bdbf2d7
Faddal Ibrahim <faddalibrahim@gmail.com>
2022-08-13 00:09:21 +0000

    on par with development

--- src/pages/protected_routes/AdminProtectedRoute.js
+++ src/pages/protected_routes/AdminProtectedRoute.js
@@ -1,11 +1,23 @@
 //libraries
 import { Navigate, Outlet } from "react-router-dom";
+import AdminTopNav from "pages/admin/admin_top_nav/AdminTopNav";
 
 const AdminProtectedRoute = () => {
   //getting admin from state management, but null for now
   const admin = true;
 
-  return <>{admin ? <Outlet /> : <Navigate to="/" />}</>;
+  return (
+    <>
+      {admin ? (
+        <div>
+          <AdminTopNav />
+          <Outlet />{" "}
+        </div>
+      ) : (
+        <Navigate to="/" />
+      )}
+    </>
+  );
 };
 
 export default AdminProtectedRoute;
