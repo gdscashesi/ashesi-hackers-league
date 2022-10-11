fb36dc5f5d9debf0cbfee42e618e8909f8d9b56f
Simon Junior Boateng <79936608+Simon-157@users.noreply.github.com>
2022-10-11 13:03:56 +0000

    Admincontext setup (#62)
    * created data model samples for questions
    
    * new sample added
    
    * set up admin context
    
    * set up dummy admin object

---
+++ src/contexts/AdminContext.js
@@ -0,0 +1,18 @@
+//libraries
+import { createContext, useState } from 'react'
+
+export const AdminContext = createContext();
+
+const AdminContextProvider = ({children}) => {
+  //waiting for json server to simulate data fetching
+  const [admin] = useState({simon:'lop'}); //dummy to cater for provider value prop
+  // setAdmin(null)
+
+  return (
+    <AdminContext.Provider value={{admin}}>
+        {children}
+    </AdminContext.Provider>
+  )
+}
+
+export default AdminContextProvider;
