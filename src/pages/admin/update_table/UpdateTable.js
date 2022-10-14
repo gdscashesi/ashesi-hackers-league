22db14e6632e23dc13bc30c55cee99ef4ee6ca6b
Simon-157 <boatengsimonjnr157@gmail.com>
2022-10-14 07:56:49 +0000

    added action btns

--- src/pages/admin/update_table/UpdateTable.js
+++ src/pages/admin/update_table/UpdateTable.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
+import Table from "./../../rankings/table/Table";
+import { AppContext } from "contexts/AppContext";
+import Button from "components/button/Button";
 
 const UpdateTable = () => {
-  return <div>Update Page</div>;
+  const { headers, scores, sortScores } = useContext(AppContext);
+  const [newHeaders, setNewHeaders] = useState(null);
+  const updateBtns = [
+    <Button text="sql" />,
+    <Button text="algo" />,
+    <Button text="script" />,
+  ];
+
+  const insertUpdateBtns = () => {
+    scores.map((score) => {
+      score["buttons"] = updateBtns;
+    });
+  };
+
+  useEffect(() => {
+    insertUpdateBtns();
+    return () => {
+      headers.push("actions");
+      setNewHeaders(headers);
+    };
+  }, [headers]);
+
+  return <Table headers={newHeaders} body={scores} sortScores={sortScores} />;
 };
 
 export default UpdateTable;
