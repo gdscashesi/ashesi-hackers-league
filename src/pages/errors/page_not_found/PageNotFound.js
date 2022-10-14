8c40d836ae6efe98a45804dc0565a71b25d07df5
Simon Junior Boateng <79936608+Simon-157@users.noreply.github.com>
2022-10-14 00:08:21 +0000

    404 inaccurate (#69)
    * added getCurrent season function
    
    * spring period extended
    
    * fixed sort function to cater for sorting based on the total score
    
    * highlt sorted column state added
    
    * 404-bug fixed

--- src/pages/errors/page_not_found/PageNotFound.js
+++ src/pages/errors/page_not_found/PageNotFound.js
@@ -9,15 +9,14 @@ import { PageNotFoundSvg } from "./PageNotFoundSvg";
 
 const PageNotFound = () => {
   const location = useLocation();
-
+  const arr = location.pathname.split("/");
   return (
     <div className={errorStyles.errorWrapper}>
       <div>
         <PageNotFoundSvg />
         <br />
         <strong>
-          sorry, looks like we don't have a{" "}
-          <span>{location.pathname.split("/")[1]} </span>
+          sorry, looks like we don't have a <span>{arr[arr.length - 1]} </span>
           page{" "}
         </strong>
       </div>
--- src/pages/rankings/table/Table.js
+++ src/pages/rankings/table/Table.js
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 import tableStyles from "./table.module.scss";
 
 const Table = ({ headers, body, sortScores }) => {
+  const [col,setCol] = useState(0);
   return (
     <div className={tableStyles.tableWrapper}>
       <table cellSpacing="0">
+        <colgroup>
+          <col span={col} style={{"background-color":"#ffff"}} />
+          <col span={1} style={{"background-color": "#FAFAFA"}} />
+        </colgroup>
         <thead>
           <tr>
             {headers?.map((headName, index) => {
               return (
                 <th
                   title="sort"
-                  onClick={() => sortScores(headName.toLowerCase())}
+                  onClick={() => {sortScores(headName.toLowerCase());setCol(index)}}
                   key={index}
                 >
                   {headName}
