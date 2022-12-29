c40e56852fdf4411026b607e90a84f99bd184927
Faddal Ibrahim <faddalibrahim@gmail.com>
2022-12-29 15:29:11 +0000

    add table reset

--- src/pages/admin/update_table/UpdateTable.js
+++ src/pages/admin/update_table/UpdateTable.js
@@ -1,82 +1,89 @@
+// libraries
 import React, { useContext, useRef, useState } from "react";
 import Table from "./../../rankings/table/Table";
-import { AppContext } from "contexts/AppContext";
 import Button from "components/button/Button";
+import { AppContext } from "contexts/AppContext";
 
 //styles
 import updateStyles from "./update.module.scss";
 import ToggleButton from "components/toggle_button/ToggleButton";
+
+//icons
+import { CaretUpIcon, UndoIcon } from "components/icons/Icons";
+
+// utils
+import { ALGORITHMS, SQL, SCRIPTING } from "utils/constants";
 
 const UpdateTable = () => {
   const { headers, scores, sortScores } = useContext(AppContext);
   const [old, setOld] = useState(false);
   const newHeaders = useRef([...headers, "actions"]);
   const [update, setUpdate] = useState([...scores]);
-  const updateScores = (score, ind, key) => {
-    update[ind][key] = update[ind][key] + 3;
+
+  const updateScores = (index, key) => {
+    update[index][key] += 3;
     setUpdate([...update]);
-    // return update;
   };
 
-  const updateBtnStyles = {
-    background: "var(--light-violet)",
-    border: "none",
-    padding: "1rem 0.8rem",
-    width: "8rem",
-    font: "inherit",
-    cursor: "pointer",
-    outline: "inherit",
-    color: "var(--text-violet)",
-    // fontWeight: "bold",
-    fontFamily: "sans-serif",
-    borderRadius: "0.25rem",
+  const undoAllScoreUpdates = () => {
+    let response = window.confirm("you are about to reset the table");
+
+    if (response) {
+      setUpdate([...JSON.parse(sessionStorage.getItem("scores"))]);
+    }
   };
 
-  const updateBtns = (score, index) => {
-    const btns = [
-      <Button
-        text="update sql"
-        styles={updateBtnStyles}
-        onClick={() => {
-          updateScores(score, index, "sql");
-        }}
-      />,
-      <Button
-        text="update algo"
-        styles={updateBtnStyles}
-        onClick={() => {
-          updateScores(score, index, "algorithms");
-        }}
-      />,
-      <Button
-        text="update script"
-        styles={updateBtnStyles}
-        onClick={() => {
-          updateScores(score, index, "scripting");
-        }}
-      />,
+  const publishScoreUpdates = () => {
+    let response = window.confirm("you are about to publish the new scores");
+
+    if (response) {
+      // async publishing to firebase
+    }
+  };
+
+  const generateUpdateBtns = (index) => {
+    return [
+      [ALGORITHMS, SCRIPTING, SQL].map((CHALLENGE_CATEGORY) => (
+        <CaretUpIcon
+          onClick={() => {
+            updateScores(index, CHALLENGE_CATEGORY);
+          }}
+        />
+      )),
     ];
-
-    return btns;
   };
 
   const insertUpdateBtns = () => {
     update.map((score, index) => {
-      return (score["buttons"] = updateBtns(score, index));
+      return (score["buttons"] = generateUpdateBtns(index));
     });
   };
   insertUpdateBtns();
 
-  const onClick = () => {
+  const toggelTableVersion = () => {
     setOld(!old);
   };
+
   return (
     <div className={updateStyles.container}>
-      <ToggleButton text="Table state" Onclick={onClick} />
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <ToggleButton text="Table state" Onclick={toggelTableVersion} />
+
+        {!old ? (
+          <div>
+            <UndoIcon onClick={undoAllScoreUpdates} />
+            <Button
+              onClick={publishScoreUpdates}
+              text="publish"
+              styles={{ background: "var(--gradient-green)" }}
+            />
+          </div>
+        ) : null}
+      </div>
       {!old ? (
         <Table
           headers={newHeaders.current}
-          body={scores}
+          body={update}
           sortScores={sortScores}
         />
       ) : (
