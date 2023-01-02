61be270b2c55671579580b83579f9ba3115f72a7
Faddal Ibrahim <faddalibrahim@gmail.com>
2023-01-02 17:37:43 +0000

    revamp admin sidebar

--- src/pages/admin/admin_top_nav/AdminSideNav.js
+++ src/pages/admin/admin_top_nav/AdminSideNav.js
@@ -1,4 +1,5 @@
 //libraries
+import { useState } from "react";
 
 //styles
 import SideNavStyles from "./admin-side-nav.module.scss";
@@ -7,16 +8,29 @@ import SideNavStyles from "./admin-side-nav.module.scss";
 import Button from "components/button/Button";
 import { NavigationButtons } from "./NavigationButtons";
 
+//utils
+import { HOME } from "utils/constants";
+
 const AdminSideNav = () => {
+  const [activePage, setActivePage] = useState(HOME);
+
   return (
     <nav className={SideNavStyles.wrapper}>
+      <h1>Admin</h1>
       {NavigationButtons?.map((button) => {
         return (
           <Button
             key={button.text}
-            icon={button.icon}
             text={button.text}
             to={button.to}
+            styles={{
+              cssText: `${
+                activePage === button.text
+                  ? "background-color: #f0f0f080; color: var(--gradient-green); font-size: 105%; font-weight: 800; border-right: 0.15rem solid var(--gradient-green)"
+                  : "background-color: initial; color: default"
+              }`,
+            }}
+            onClick={() => setActivePage(button.text)}
           />
         );
       })}
