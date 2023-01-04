8ad8ee5bc2bebb818f2e1048d5ea5551b8b82067
Simon-157 <boatengsimonjnr157@gmail.com>
2023-01-04 15:24:52 +0000

    rules revamped

--- src/pages/instructions/Instructions.js
+++ src/pages/instructions/Instructions.js
@@ -2,6 +2,7 @@ import React from "react";
 import instructionsStyles from "./instructionsStyles.module.scss";
 import Button from "components/button/Button";
 import { CHALLENGES } from "utils/constants";
+import { instructions } from "utils/rulesdata";
 
 const Instructions = () => {
   return (
@@ -14,95 +15,18 @@ const Instructions = () => {
       </div>
 
       <div className={instructionsStyles.container}>
-        {/* <div className={instructionsStyles.divider}></div> */}
-        <div>
-          <h3>General Rules</h3>
-          <p>
-            <li>Start by understanding the problem statement:</li>
-            <li>
-              Identify the key constraints: Look for any constraints in the
-              problem that may affect your solution.
-            </li>
-            <li>
-              Identify the key constraints: Look for any constraints in the
-              problem that may affect your solution.
-            </li>
-            <li>
-              Look for patterns: Many algorithm problems can be solved by
-              identifying patterns in the data.
-            </li>{" "}
-            <li>
-              Consider different approaches: There may be multiple ways to solve
-              an algorithm problem.
-            </li>
-          </p>
-        </div>
-        <div>
-          <h3>Points Accumulation</h3>
-          <p>
-            <li>Start by understanding the problem statement:</li>
-            <li>
-              Identify the key constraints: Look for any constraints in the
-              problem that may affect your solution.
-            </li>
-            <li>
-              Identify the key constraints: Look for any constraints in the
-              problem that may affect your solution.
-            </li>
-            <li>
-              Look for patterns: Many algorithm problems can be solved by
-              identifying patterns in the data.
-            </li>{" "}
-            <li>
-              Consider different approaches: There may be multiple ways to solve
-              an algorithm problem.
-            </li>
-          </p>
-        </div>
-        <div>
-          <h3>Teams Rules</h3>
-          <p>
-            <li>Start by understanding the problem statement:</li>
-            <li>
-              Identify the key constraints: Look for any constraints in the
-              problem that may affect your solution.
-            </li>
-            <li>
-              Identify the key constraints: Look for any constraints in the
-              problem that may affect your solution.
-            </li>
-            <li>
-              Look for patterns: Many algorithm problems can be solved by
-              identifying patterns in the data.
-            </li>{" "}
-            <li>
-              Consider different approaches: There may be multiple ways to solve
-              a challenge.
-            </li>
-          </p>
-        </div>
-        <div>
-          <h3>Other Rules</h3>
-          <p>
-            <li>Start by understanding the problem statement:</li>
-            <li>
-              Identify the key constraints: Look for any constraints in the
-              problem that may affect your solution.
-            </li>
-            <li>
-              Identify the key constraints: Look for any constraints in the
-              problem that may affect your solution.
-            </li>
-            <li>
-              Look for patterns: Many algorithm problems can be solved by
-              identifying patterns in the data.
-            </li>{" "}
-            <li>
-              Consider different approaches: There may be multiple ways to solve
-              a challenge
-            </li>
-          </p>
-        </div>
+        {instructions?.map((instruction) => {
+          return (
+            <div>
+              <h3>{instruction.header}</h3>
+              <p>
+                {instruction.rules.map((rule) => {
+                  return <li>{rule}</li>;
+                })}
+              </p>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
